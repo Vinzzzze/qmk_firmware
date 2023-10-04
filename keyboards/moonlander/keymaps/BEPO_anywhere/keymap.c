8c7a36b26107ed5eb57ef02b111ebe09b706d859
coolcoyote <coolcoyote358@gmail.com>
2023-10-05 00:46:18 +0200

    Update the led management

--- keyboards/moonlander/keymaps/BEPO_anywhere/keymap.c
+++ keyboards/moonlander/keymaps/BEPO_anywhere/keymap.c
@@ -2581,3 +2581,68 @@ bool caps_word_press_user(uint16_t keycode) {
 
    return false;
 }
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        ML_LED_5(led_state.num_lock);
+        ML_LED_6(led_state.caps_lock);
+    }
+    return res;
+}
+
+extern bool is_launching;
+
+layer_state_t layer_state_set_kb(layer_state_t state) {
+    state = layer_state_set_user(state);
+    if (is_launching || !keyboard_config.led_level) return state;
+    bool LED_1 = false;
+    bool LED_2 = false;
+    bool LED_3 = false;
+    bool LED_4 = false;
+
+    uint8_t layer = get_highest_layer(state);
+    switch (layer) {
+        case 1:
+            LED_4 = true;
+            break;
+        case 2:
+            LED_3 = true;
+            break;
+        case 3:
+            LED_1 = true;
+            break;
+        case 4:
+            LED_1 = true;
+            LED_4 = true;
+            break;
+        case 5:
+            LED_1 = true;
+            LED_3 = true;
+            break;
+        case 6:
+            LED_2 = true;
+            break;
+        case 7:
+            LED_2 = true;
+            LED_4 = true;
+            break;
+        case 8:
+            LED_2 = true;
+            LED_3 = true;
+            break;
+        case 9:
+            LED_1 = true;
+            LED_2 = true;
+            break;
+        default:
+            break;
+    }
+
+    ML_LED_1(LED_1);
+    ML_LED_2(LED_2);
+    ML_LED_3(LED_3);
+    ML_LED_4(LED_4);
+
+    return state;
+}
